341fa38f66fd2c7c9c3c2348e1f75aa4ab3678a6
Diego Revilla <string.diego.revilla@gmail.com>
2022-07-01 23:36:54 -0500

    Adding heroes cards

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,20 +1,20 @@
-import React, { useMemo } from 'react'
-import { getHeroesByPublisher } from '../helpers';
+import React, { useMemo } from "react";
+import { getHeroesByPublisher } from "../helpers";
+import { HeroCard } from "./HeroCard";
 
-export const HeroList = ({publisher}) => {
+export const HeroList = ({ publisher }) => {
+	// const heroes = getHeroesByPublisher(publisher);
 
-    // const heroes = getHeroesByPublisher(publisher);
+	const heroes = useMemo(
+		() => getHeroesByPublisher(publisher),
+		[publisher]
+	);
 
-    const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
-
-  return (
-    <ul>
-        {
-            heroes.map((heroe) => 
-                <li key={heroe.id}>
-                    {heroe.superhero}
-                </li>)
-        }
-    </ul>
-  )
-}
+	return (
+		<div className="row rows-cols-1 row-cols-md-3 g-3">
+			{heroes.map((heroe) => (
+				<HeroCard key={heroe.id} {...heroe} />
+			))}
+		</div>
+	);
+};
